4d5a3f3250f46ce7a893032b28d082fc090a9ab8
LuisESouza <leduardosouza2204@gmail.com>
2025-03-14 02:58:01 -0400

    add getUserById and update createUser function

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,12 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from 'bcryptjs';
 const prisma = new PrismaClient();
 
-export const createUser = async(data: { username: string; password: string;}) =>{
+export const createUser = async(data: { email: string, username: string; password: string; }) => {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
     return await prisma.users.create({
-        data:{
+        data: {
+            email: data.email,
             username: data.username,
-            password: data.password,
-            created_at: new Date()
+            password: hashedPassword,
+            created_at: new Date(),
         }
     });
 };
@@ -14,5 +17,11 @@ export const createUser = async(data: { username: string; password: string;}) =>
 export const getUserById = async (user_id: string) => {
     return await prisma.users.findUnique({
         where: { id: user_id },
+    });
+};
+
+export const getUserByEmail = async (email: string) => {
+    return prisma.users.findUnique({
+        where: { email },
     });
 };
