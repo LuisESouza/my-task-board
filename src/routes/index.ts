52f0011e3efb64c450ba0f45bffba80ccc8b465d
LuisESouza <leduardosouza2204@gmail.com>
2025-03-14 02:49:47 -0400

    Update routes index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import tasksRouter from './tasksRoutes';
+import taskRouter from './taskRoutes';
 import userRouter from './userRoutes';
 
 const router = express.Router();
 
 // TASKS ROUTES
-router.use('/tasks', tasksRouter);
+router.use('/tasks', taskRouter);
 
 // USERS ROUTES
 router.use('/user', userRouter);
