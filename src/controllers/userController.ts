eaf2d22a2b789f8eea9da4723cf1df8fb5dd58a0
LuisESouza <leduardosouza2204@gmail.com>
2025-03-14 02:53:09 -0400

    Add function login

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,15 +1,21 @@
 import { Request, Response } from 'express';
 import * as UserModel from '../models/userModel';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
-export const createUser = async(req: Request, res: Response) => {
-    const {  username, password } = req.body;
+export const createUser = async(req: Request, res: Response): Promise<any> => {
+    const {  email, username, password } = req.body;
     try{
-        const user = await UserModel.createUser({ username, password});
+        const userCheck = await UserModel.getUserByEmail(email)
+        if(userCheck){
+          return res.status(500).json('Erro ao registrar: Email ja registrado');
+        }
+        const user = await UserModel.createUser({ email ,username, password});
         res.status(201).json(user);
     }catch(error){
-        res.status(500).json({message: 'Erro ao criar user', error});
+        res.status(500).json({message: 'Erro ao registrar user', error});
     }
-}
+};
 
 export const getUserById = async (req: Request, res: Response): Promise<any> => {
   const { user_id } = req.params;
@@ -21,5 +27,23 @@ export const getUserById = async (req: Request, res: Response): Promise<any> => 
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ message: 'Erro ao buscar usuário', error });
+  }
+};
+
+export const loginUser = async(req: Request, res: Response): Promise<any> => {
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.getUserByEmail(email);
+    if (!user) {
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ message: 'Senha inválida' });
+    }
+    const token = jwt.sign({ userId: user.id }, 'teste', { expiresIn: '1h' });
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao fazer login', error });
   }
 };
